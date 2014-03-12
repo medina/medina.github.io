20d06d593d1eae37b31cf449c34ce0798e3fd7a6
medina <medina@mongodb.com>
2014-03-12 19:12:49 -0400

    sparkline testing

---
+++ javascripts/sparkline.js
@@ -0,0 +1,6 @@
+$(function() {
+    /** This code runs when everything has been loaded on the page */
+    /* Inline sparklines take their values from the contents of the tag */
+    $('.inlinesparkline').sparkline();
+
+});
