f1dc2abd61c9feef99919aae9729efbc770898c5
medina <daniel.medina@gmail.com>
2017-01-30 23:11:49 -0500

    burn it all

--- javascripts/sparkline.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    /** This code runs when everything has been loaded on the page */
-    /* Inline sparklines take their values from the contents of the tag */
-    $('.inlinesparkline').sparkline();
-
-});
